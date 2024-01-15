616e5560dd180e129ae6a2ff1818cf3b6c1aa378
mohamadtshehab <iammohamadshehab@gmail.com>
2024-01-15 23:26:50 +0200

    added while and del statements

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-def sami():
+def x():
     y = 10
-    s = 20
-    if 1 > 0:
-        s.a().m = 1
-    elif 2 > 1:
-        s.a().m = 2
-    else:
-        s.a().m = 3
-    def jafar():
-        y = 3
+    oa = 20
+    
