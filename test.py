09d79118eebb5d2cf391e524d42ae62bd47d2474
Sami Alomar <sami.anas.alomar.2016.king@gmail.com>
2024-01-16 06:33:18 +0300

    scroll up and down

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ class MyClass:
         match x:
             case 1:
                 return x + 1
-                return y
-                return z
+            case 2:
+                return w + 2
+            case 1:
+                assert x + 1
             case 2:
                 return w + 2
